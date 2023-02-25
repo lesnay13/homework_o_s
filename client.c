9465f7f2b7fa91db750daf92da3e6231191d33b3
Yansel F <55860487+lesnay13@users.noreply.github.com>
2023-02-24 19:26:17 -0600

    initial commit

--- client.c
+++ client.c
@@ -1,33 +1,217 @@
 #include <stdio.h>
 #include <string.h>
-
-#include <unistd.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+// Server Fifo
+#define REQUEST_FIFO "request.fifo"
 
-int main(int argc, char* argv[])
+// declarations
+char my_fifo_name [128];
+int connect_to_server(int, int, int);
+int client_request(int, int, int);
+void exit_request(int, int, int);
+void terminate_request(int, int, int);
+
+// main method
+int main()
 {
-    int fds[2];
-    char *fifo = "/tmp/server";
-    char *fifo2 = "/tmp/client";
+    int case_number;
+    int loop_counter= 1;
+    int request_fifo_fd;  // server fifo
+    int response_fifo_fd; // client fifo
 
-    mkfifo(fifo2,0666);
-    fds[0]=open(fifo,O_RDONLY);
-    fds[1]=open(fifo2,O_WRONLY);
+    pid_t pid;
 
-    char tab[BUFSIZ];
-    memset(tab, 0, sizeof(tab));
+    int system_call_number;
+    int num_params;
+    int size_params;
 
-    write(fds[1],"Client",6);
-    perror("Write:"); //Very crude error check
-    read(fds[0],tab,sizeof(tab));
-    perror("Read:"); // Very crude error check
+    // Get the process ID
+    pid = getpid();
 
-    printf("Message from server: %s\n",tab);
+    // Connect to server FIFO, create client FIFO, and send connection info to server
+    if (connect_to_server(request_fifo_fd, response_fifo_fd, pid) != 0)
+    {
+        perror("Connection failure");
+        return 1;
+    }
 
-    close(fds[0]);
-    close(fds[1]);
-    unlink(fifo2);
+    // Inifite loop to ask user what to do next
+    while (loop_counter)
+    {
+        printf("What to do now?\n");
+        printf("1. Send a request to server\n2. Exit\n3. Terminate\n");
+        scanf("%d", &case_number);
+        //Switch between user choices
+        switch (case_number)
+        {
+            case 1:
+                client_request(request_fifo_fd, response_fifo_fd, pid);
+                break;
+
+            case 2:
+                // Exit, this client doesnt want to issue more requests
+                exit_request(request_fifo_fd, response_fifo_fd, pid);
+                loop_counter = 0;
+                break;
+
+            case 3:
+                // Terminate, this client doesnt want to issue more requests and signals server to term
+                terminate_request(request_fifo_fd, response_fifo_fd, pid);
+
+                loop_counter = 0;
+                break;
+
+            default:
+                printf("Error: Incorrect selection. What to do now?");
+                printf("1. Send a request to server  2. Exit  3. Terminate");
+                scanf("%d", &case_number);
+                break;
+        }      
+    }
+    // Close the request & response FIFO queue
+    close(request_fifo_fd);
+    unlink(REQUEST_FIFO);
+    close(response_fifo_fd);
+    unlink(my_fifo_name);
+
     return 0;
+}
+
+// function definitions
+
+// Create the client-specific FIFO using an appropriate name (e.g., “./ClientNfifo”), 
+// where N is the client number and send the initial “connect system call” to the server 
+// including client number and name of the client-specific FIFO.
+int connect_to_server(int request_fifo_fd, int response_fifo_fd, int pid)
+{
+    // hard-coded because this is first request by client ("connect system call")
+    int system_call_number = 1;
+    int num_params = 1;
+    int size_params;
+
+    // Create client fifo utilizing process id
+    sprintf(my_fifo_name, "./client%dfifo", pid);
+    if (mkfifo (my_fifo_name, 0666) == -1)
+    {  
+        perror ("mkfifo");
+    }
+    printf("Created client FIFO... \n");
+
+    // Open the request FIFO queue for writing
+    request_fifo_fd = open(REQUEST_FIFO, O_WRONLY);
+    if (request_fifo_fd < 0)
+    {
+        perror("open");
+        return 1;
+    }
+    printf("Opened server FIFO to write... \n");
+
+    // Calculate the size of the rest of the message
+    size_params = sizeof(my_fifo_name)*2;
+
+    //Hard coded 1 system call
+    write(request_fifo_fd, &pid, sizeof(pid_t));
+    write(request_fifo_fd, &system_call_number, sizeof(int));
+    write(request_fifo_fd, &num_params, sizeof(int));
+    write(request_fifo_fd, &size_params, sizeof(int));
+    write(request_fifo_fd, &my_fifo_name, sizeof(char));
+    printf("Sent initial request containing client FIFO [%s]]... \n", my_fifo_name);
+    
+
+    printf("Opening client FIFO for reading... \n");
+    // Open the response/client FIFO queue for reading
+    response_fifo_fd = open(my_fifo_name, O_RDONLY);
+    if (response_fifo_fd < 0)
+    {
+        perror("open");
+        return 1;
+    }
+
+    printf("Connection to server established... \n");
+
+    return 0;
+}
+
+// TODO: <copy requirements from homework doc here>
+int client_request(int request_fifo_fd, int response_fifo_fd, int pid)
+{
+    int system_call_number;
+    int num_params;
+    int size_params;
+
+    //Send request to server
+    printf("Please choose what system call to do: \n");
+    printf("1. Connect to server\n2. Convert number to text \n3. Convert Text to Number\n");
+    printf("4. Store\n5. Recall\n0. Exit\n-1. Terminate \n");
+
+    // Read system call information from the user
+    printf("Enter system call number: \n");
+    scanf("%d", &system_call_number);
+    printf("Enter number of parameters: \n");
+    scanf("%d", &num_params);
+
+    // Calculate the size of the rest of the message
+    size_params = num_params * sizeof(int);
+
+    // Write the system call information to the request FIFO queue
+    write(request_fifo_fd, &pid, sizeof(pid_t));
+    write(request_fifo_fd, &system_call_number, sizeof(int));
+    write(request_fifo_fd, &num_params, sizeof(int));
+    write(request_fifo_fd, &size_params, sizeof(int));
+
+    // Write the parameters to the request FIFO queue
+    for (int i = 0; i < num_params; i++)
+    {
+        int param;
+        printf("Enter parameter %d: \n", i + 1);
+        scanf("%d", &param);
+        write(request_fifo_fd, &param, sizeof(int));
+    }
+    printf("Sent request");
+
+    // TODO: <how do we return system call information?>
+    return 0;
+}
+
+// EXIT - indicates THIS client does not want to issue more requests to the server,
+// it should send a “EXIT” system call to the server, 
+// close its client specific FIFO, delete it and exit.
+void exit_request(int request_fifo_fd, int response_fifo_fd, int pid)
+{
+    // hard-coded because this is exit request by client
+    int system_call_number = 0;
+    int num_params = 0;
+    int size_params = 0;
+
+    //Hard coded 1 system call
+    write(request_fifo_fd, &pid, sizeof(pid_t));
+    write(request_fifo_fd, &system_call_number, sizeof(int));
+    write(request_fifo_fd, &num_params, sizeof(int));
+    write(request_fifo_fd, &size_params, sizeof(int));
+    write(request_fifo_fd, &my_fifo_name, sizeof(int));
+    printf("Sent exit request to server... \n");
+}
+
+// TERMINATE - indicates THIS client does not want to issue more requests to the server, 
+// and is flagging the server to also exit. it should send a “TERMINATE” system call to 
+// the server, close its client specific FIFO, delete it and exit.
+void terminate_request(int request_fifo_fd, int response_fifo_fd, int pid)
+{
+    // hard-coded because this is exit request by client
+    int system_call_number = -1;
+    int num_params = 0;
+    int size_params = 0;
+
+    //Hard coded 1 system call
+    write(request_fifo_fd, &pid, sizeof(pid_t));
+    write(request_fifo_fd, &system_call_number, sizeof(int));
+    write(request_fifo_fd, &num_params, sizeof(int));
+    write(request_fifo_fd, &size_params, sizeof(int));
+    write(request_fifo_fd, &my_fifo_name, sizeof(int));
+    printf("Sent terminate request to server... \n");
 }
--- server.c
+++ server.c
@@ -1,62 +1,260 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <stdarg.h>
+#include <stdlib.h>
 
-#define port 8088
+// Server Fifo
+#define REQUEST_FIFO "request.fifo"
 
-int main(int argc, char* argv[])
+// declarations
+char my_fifo_name [128];
+
+int connect_system(pid_t, int);
+void num_to_text(char*);
+void text_to_num();
+void store(char*,int);
+void recall();
+void terminate_connection();
+
+int main()
 {
-    int fds[2];
-    char tab[BUFSIZ];
-    int fd, n;
+    int request_fifo_fd;
+    int *client_fifo_name;
+    int response_fifo_fd;
+    pid_t client_pid;
+    int system_call_number;
+    int num_params;
+    int size_params;
+    char *params;
+    char *num;
 
-    char *fifo1 = "/tmp/server";
-    char *fifo2 = "/tmp/client";
+    // Create the well-known request FIFO queue
+    mkfifo(REQUEST_FIFO, 0666);
 
-    pipe(fds);
-    mkfifo(fifo1,0666);
-
-    while(1)
+    // Open the request FIFO queue for reading
+    request_fifo_fd = open(REQUEST_FIFO, O_RDONLY);
+    if (request_fifo_fd < 0)
     {
-        fds[0]=open(fifo2,O_RDONLY);
-        fds[1]=open(fifo1,O_WRONLY);
-
-        read(fds[0],tab,BUFSIZ);
-
-        if (strcmp("Client",tab)==0) {
-            printf("Client Connection: %s\n",tab);
-            fd=open(tab,O_WRONLY);
-
-            if(fork()==0)
-            {
-                dup2(fds[1],1);
-                close(fds[1]);
-                execlp("ls","ls","-l",NULL);
-                close(fds[0]);
-                close(fds[1]);
-            }
-            else
-            {
-                dup2(fds[0],0);
-                n = read(fds[0],tab,BUFSIZ);
-                write(fd,tab,n);
-                close(fds[0]);
-                close(fds[1]);
-            }
-        }
-        memset(tab, 0, sizeof(tab));
-        close(fd);
-        close(fds[0]);
-        close(fds[1]);
+        perror("open");
+        return 1;
     }
 
-    unlink(fifo1);
+    // Enter an infinite loop to process client requests
+    while (1)
+    {
+
+        printf("Received request from client in well-known FIFO... \n");
+        // Read the client PID, system call number, and number of parameters
+        read(request_fifo_fd, &client_pid, sizeof(pid_t));
+        read(request_fifo_fd, &system_call_number, sizeof(int));
+        read(request_fifo_fd, &num_params, sizeof(int));
+        read(request_fifo_fd, &size_params, sizeof(int));
+
+        // Allocate memory for the parameters
+        params = (char *)malloc(size_params*2);
+
+        printf("Trying to read params...\n");
+        read(request_fifo_fd, &params, sizeof(int));
+        printf("Received %ls \n", &params);
+        // Read the parameters
+        /*for (int i = 0; i < num_params; i++)
+        {
+            printf("Loop 1\n");
+
+            read(request_fifo_fd, &params[i], sizeof(int));
+            printf("Received %s \n", &params[i]);
+        }*/
+    
+
+        printf("System call received from client is: %d\n", system_call_number);
+        printf("Client pid: %d\n", client_pid);
+        printf("System call requested: %d\n", system_call_number);
+        printf(" with %d", num_params);
+        printf(" parameters which are: \n");
+        printf(" size of params: %d\n", size_params);
+
+
+
+        // Perform the requested system call (in this case, just convert and store the parameters)
+        switch (system_call_number)
+        {
+            case -1:
+                terminate_connection();
+                break;
+            case 0:
+                exit(0);
+                break;
+            case 1:
+                connect_system(client_pid, response_fifo_fd);
+                break;
+            case 2:
+                num_to_text(num);
+                break;
+            case 3:
+                text_to_num();
+                break;
+            case 4:
+                //store(params, num_params);
+                break;
+            case 5:
+                recall();
+                break;
+
+            default:
+                break;
+        }
+        // Write the converted parameters back to the client
+        write(response_fifo_fd, &num_params, sizeof(int));
+        write(response_fifo_fd, &size_params, sizeof(int));
+        write(response_fifo_fd, params, size_params);
+
+        // Close the response FIFO queue
+        close(response_fifo_fd);
+        unlink(my_fifo_name);
+
+        // Free the memory for the parameters
+        free(params);
+        free(num);
+    }
+
+    // Close the request FIFO queue
+    close(request_fifo_fd);
+    unlink(REQUEST_FIFO);
     return 0;
+}
+
+int connect_system(pid_t client_pid, int response_fifo_fd)
+{
+    // Open the response FIFO queue for writing
+    char response_fifo_name[200];
+    sprintf(response_fifo_name, "%d.%s", client_pid, my_fifo_name);
+    response_fifo_fd = open(response_fifo_name, O_WRONLY);
+    if (response_fifo_fd < 0)
+    {
+        perror("open");
+        return 1;
+    }
+    printf("Opened client FIFO for writing...\n");
+    return 0;
+}
+
+void num_to_text(char* num)
+{
+    int len = strlen(num); // Get number of digits in given number
+ 
+    /* Base cases */
+    if (len == 0) {
+        fprintf(stderr, "empty string\n");
+        return;
+    }
+    if (len > 4) {
+        fprintf(stderr,
+                "Length more than 4 is not supported\n");
+        return;
+    }
+ 
+    /* The first string is not used, it is to make
+        array indexing simple */
+    char* single_digits[]
+        = { "zero", "one", "two",   "three", "four",
+            "five", "six", "seven", "eight", "nine" };
+ 
+    /* The first string is not used, it is to make
+        array indexing simple */
+    char* two_digits[]
+        = { "",          "ten",      "eleven",  "twelve",
+            "thirteen",  "fourteen", "fifteen", "sixteen",
+            "seventeen", "eighteen", "nineteen" };
+ 
+    /* The first two string are not used, they are to make
+        array indexing simple*/
+    char* tens_multiple[] = { "",       "",        "twenty",
+                              "thirty", "forty",   "fifty",
+                              "sixty",  "seventy", "eighty",
+                              "ninety" };
+ 
+    char* tens_power[] = { "hundred", "thousand" };
+ 
+    /* Used for debugging purpose only */
+    printf("\n%s: ", num);
+ 
+    /* For single digit number */
+    if (len == 1) {
+        printf("%s\n", single_digits[*num - '0']);
+        return;
+    }
+ 
+    /* Iterate while num is not '\0' */
+    while (*num != '\0') {
+ 
+        /* Code path for first 2 digits */
+        if (len >= 3) {
+            if (*num - '0' != 0) {
+                printf("%s ", single_digits[*num - '0']);
+                printf("%s ",
+                       tens_power[len - 3]); // here len can
+                                             // be 3 or 4
+            }
+            --len;
+        }
+ 
+        /* Code path for last 2 digits */
+        else {
+            /* Need to explicitly handle 10-19. Sum of the
+            two digits is used as index of "two_digits"
+            array of strings */
+            if (*num == '1') {
+                int sum = *num - '0' + *(num + 1) - '0';
+                printf("%s\n", two_digits[sum]);
+                return;
+            }
+ 
+            /* Need to explicitly handle 20 */
+            else if (*num == '2' && *(num + 1) == '0') {
+                printf("twenty\n");
+                return;
+            }
+ 
+            /* Rest of the two digit numbers i.e., 21 to 99
+             */
+            else {
+                int i = *num - '0';
+                printf("%s ", i ? tens_multiple[i] : "");
+                ++num;
+                if (*num != '0')
+                    printf("%s ",
+                           single_digits[*num - '0']);
+            }
+        }
+        ++num;
+    }
+    free(num);
+}
+
+void text_to_num()
+{
+    // int size= sizeof(text);
+    // int numConverted = atoi(text);
+
+    // printf("Converting text to number:  %d\n", numConverted);
+}
+
+void store(char *params, int num_params)
+{
+    /*for (int i = 0; i < num_params; i++)
+    {
+        params[i] = params[i] * 2;
+    }*/
+}
+void recall()
+{
+
+}
+
+void terminate_connection()
+{
+
 }
