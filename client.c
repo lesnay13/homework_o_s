fde52190a120c30ee881b0f25da2c8523798280a
Yansel Flores <yansel.13@hotmail.com>
2023-03-10 05:26:35 -0600

    deleted file and adjusted names files

--- client.c
+++ client.c
@@ -1,217 +1,218 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
-// Server Fifo
-#define REQUEST_FIFO "request.fifo"
+#define MAX_PARAM_LENGTH 256
+#define SERVER_FIFO_NAME "./server_fifo"
+#define CLIENT_FIFO_PREFIX "./client_fifo_"
 
-// declarations
-char my_fifo_name [128];
-int connect_to_server(int, int, int);
-int client_request(int, int, int);
-void exit_request(int, int, int);
-void terminate_request(int, int, int);
+// Useful for debugging
+void pid_printf(const char* format, ...) {
+    pid_t pid = getpid();
+    va_list args;
+    va_start(args, format);
+    fprintf(stdout, "[PID %d] - ", pid);
+    vfprintf(stdout, format, args);
+    va_end(args);
+}
 
-// main method
-int main()
-{
-    int case_number;
-    int loop_counter= 1;
-    int request_fifo_fd;  // server fifo
-    int response_fifo_fd; // client fifo
 
-    pid_t pid;
-
+int main() {
+    // Declaring variables
     int system_call_number;
     int num_params;
     int size_params;
+    int case_number;
+    char optional_param[MAX_PARAM_LENGTH];
+    char empty_string[] = "";
+    int loop_continue = 1;
+
 
     // Get the process ID
+    pid_t pid;
     pid = getpid();
 
-    // Connect to server FIFO, create client FIFO, and send connection info to server
-    if (connect_to_server(request_fifo_fd, response_fifo_fd, pid) != 0)
-    {
-        perror("Connection failure");
+    // Open server FIFO for writing
+    pid_printf("Opening server FIFO: %s\n", SERVER_FIFO_NAME);
+    int server_fifo_fd = open(SERVER_FIFO_NAME, O_WRONLY);
+    if (server_fifo_fd == -1) {
+        perror("Server fifo doesn't exist\n");
         return 1;
     }
 
+    // Creating our FIFO so the server can attempt to open it when it reads the request
+    pid_printf("Creating client FIFO\n");
+    char client_fifo_name[MAX_PARAM_LENGTH];
+    sprintf(client_fifo_name, CLIENT_FIFO_PREFIX"%d", pid);
+    if (mkfifo (client_fifo_name, 0666) == -1)
+    {  
+        perror ("Error making client fifo\n");
+    }
+
+    // Calculate the size of the rest of the message
+    // hardcode other params for connect request
+    system_call_number = 1;
+    num_params = 1;
+    size_params = sizeof(client_fifo_name);
+
+    // Writing the params for the connect request to the server FIFO
+    pid_printf("Writing connect request to server FIFO\n");
+    write(server_fifo_fd, &pid, sizeof(pid));
+    write(server_fifo_fd, &system_call_number, sizeof(system_call_number));
+    write(server_fifo_fd, &num_params, sizeof(num_params));
+    write(server_fifo_fd, &size_params, sizeof(size_params));
+    write(server_fifo_fd, &client_fifo_name, sizeof(client_fifo_name));
+
+    // Open client FIFO for reading
+    pid_printf("opening client FIFO: %s\n", client_fifo_name);
+    int client_fifo_fd = open(client_fifo_name, O_RDONLY);
+    if (client_fifo_fd == -1) {
+        perror("client FIFO open error\n");
+        return 1;
+    }
+
+    // Read server response from client FIFO
+    pid_printf("Reading server response from client FIFO\n");
+    char server_response[MAX_PARAM_LENGTH];
+    read(client_fifo_fd, server_response, MAX_PARAM_LENGTH);
+    pid_printf("Server response: '%s'\n", server_response);
+
+
+// ------------------------------
+// TODO: Yannie edit replace/here
+pid_printf("Connection to server established... \n");
+
     // Inifite loop to ask user what to do next
-    while (loop_counter)
+    while (loop_continue)
     {
         printf("What to do now?\n");
         printf("1. Send a request to server\n2. Exit\n3. Terminate\n");
         scanf("%d", &case_number);
+
         //Switch between user choices
         switch (case_number)
         {
             case 1:
-                client_request(request_fifo_fd, response_fifo_fd, pid);
+                pid_printf("Client request chosen... \n");
+
+                //Send request to server
+                printf("Please choose what system call to do: \n");
+                printf("1. Connect to server\n2. Convert number to text \n3. Convert Text to Number\n");
+                printf("4. Store\n5. Recall\n0. Exit\n-1. Terminate \n");
+
+                // Read system call information from the user
+                printf("Enter system call number: \n");
+                scanf("%d", &system_call_number);
+                printf("Enter number of parameters [0 or 1]: \n");
+                scanf("%d", &num_params);
+
+                // According to project spec we only require one or zero parameters right now
+                if (num_params != 0 && num_params > 1) {
+                    // Wrong value, start loop over
+                    pid_printf("Error: Number of parameters must equal 0 or 1.");
+                    continue;
+                }
+
+                if (num_params == 1) {
+                    // read in user input and store in optional param as string
+                    // can change the data type on server side if it is an int
+                    printf("Enter parameter value: \n");
+                    fflush(stdout); // Flush the output buffer to ensure prompt is displayed
+                    //fgets(optional_param, MAX_PARAM_LENGTH, stdin);
+                    scanf("%s", optional_param);
+                   
+
+                    // fgets adds a newline, so replace it with string termination null char
+                   optional_param[strcspn(optional_param, "\n")] = '\0';
+                    printf("You entered: %s\n", optional_param);
+                }
+
+                if (num_params == 0) {
+                    // if the client doesn't want to enter a param just make it equal empty string
+                    sprintf(optional_param, "%s", empty_string);
+                }
+
+                // Write the system call information to the request FIFO queue
+                write(server_fifo_fd, &pid, sizeof(pid_t));
+                write(server_fifo_fd, &system_call_number, sizeof(int));
+                write(server_fifo_fd, &num_params, sizeof(int));
+                write(server_fifo_fd, &size_params, sizeof(int));
+                write(server_fifo_fd, &optional_param, sizeof(optional_param));
+                pid_printf("Sent client request to server... \n");
+                
                 break;
 
+            // EXIT - indicates THIS client does not want to issue more requests to the server,
+            // it should send a “EXIT” system call to the server, 
+            // close its client specific FIFO, delete it and exit.
             case 2:
                 // Exit, this client doesnt want to issue more requests
-                exit_request(request_fifo_fd, response_fifo_fd, pid);
-                loop_counter = 0;
+                pid_printf("Exit request chosen... \n");
+
+                //Hard coded vars for exit request
+                sprintf(optional_param, "%s", empty_string);
+                system_call_number = 0;
+                num_params = 0;
+                size_params = sizeof(optional_param);
+                
+                // Hard coded call
+                write(server_fifo_fd, &pid, sizeof(pid_t));
+                write(server_fifo_fd, &system_call_number, sizeof(int));
+                write(server_fifo_fd, &num_params, sizeof(int));
+                write(server_fifo_fd, &size_params, sizeof(int));
+                write(server_fifo_fd, &optional_param, sizeof(optional_param));
+                pid_printf("Sent exit request to server... \n");
+
+                // exit the loop via var
+                // break here just ends the switch, which is needed to avoid default behavior
+                loop_continue = 0;
                 break;
 
+            // TERMINATE - indicates THIS client does not want to issue more requests to the server, 
+            // and is flagging the server to also exit. it should send a “TERMINATE” system call to 
+            // the server, close its client specific FIFO, delete it and exit.
             case 3:
                 // Terminate, this client doesnt want to issue more requests and signals server to term
-                terminate_request(request_fifo_fd, response_fifo_fd, pid);
+                pid_printf("Terminate request chosen... \n");
 
-                loop_counter = 0;
+                //Hard coded vars for term request
+                sprintf(optional_param, "%s", empty_string);
+                system_call_number = -1;
+                num_params = 0;
+                size_params = sizeof(optional_param);
+                
+                // Hard coded call
+                write(server_fifo_fd, &pid, sizeof(pid_t));
+                write(server_fifo_fd, &system_call_number, sizeof(int));
+                write(server_fifo_fd, &num_params, sizeof(int));
+                write(server_fifo_fd, &size_params, sizeof(int));
+                write(server_fifo_fd, &optional_param, sizeof(optional_param));
+                pid_printf("Sent terminate request to server... \n");
+
+                // exit the loop via var
+                // break here just ends the switch, which is needed to avoid default behavior
+                loop_continue = 0;
                 break;
 
             default:
-                printf("Error: Incorrect selection. What to do now?");
-                printf("1. Send a request to server  2. Exit  3. Terminate");
-                scanf("%d", &case_number);
-                break;
+                pid_printf("Error: Incorrect case selection.\n");
+                continue;
         }      
     }
-    // Close the request & response FIFO queue
-    close(request_fifo_fd);
-    unlink(REQUEST_FIFO);
-    close(response_fifo_fd);
-    unlink(my_fifo_name);
 
+// ------------------------------
+
+    // Close FIFO and unlink because we are done
+    close(client_fifo_fd);
+    unlink(client_fifo_name);
+
+    // If I unlink the server here it seems to close it for everyone, so not sure how to handle it yet
+    close(server_fifo_fd);
+
+    // Exit main
     return 0;
-}
-
-// function definitions
-
-// Create the client-specific FIFO using an appropriate name (e.g., “./ClientNfifo”), 
-// where N is the client number and send the initial “connect system call” to the server 
-// including client number and name of the client-specific FIFO.
-int connect_to_server(int request_fifo_fd, int response_fifo_fd, int pid)
-{
-    // hard-coded because this is first request by client ("connect system call")
-    int system_call_number = 1;
-    int num_params = 1;
-    int size_params;
-
-    // Create client fifo utilizing process id
-    sprintf(my_fifo_name, "./client%dfifo", pid);
-    if (mkfifo (my_fifo_name, 0666) == -1)
-    {  
-        perror ("mkfifo");
-    }
-    printf("Created client FIFO... \n");
-
-    // Open the request FIFO queue for writing
-    request_fifo_fd = open(REQUEST_FIFO, O_WRONLY);
-    if (request_fifo_fd < 0)
-    {
-        perror("open");
-        return 1;
-    }
-    printf("Opened server FIFO to write... \n");
-
-    // Calculate the size of the rest of the message
-    size_params = sizeof(my_fifo_name)*2;
-
-    //Hard coded 1 system call
-    write(request_fifo_fd, &pid, sizeof(pid_t));
-    write(request_fifo_fd, &system_call_number, sizeof(int));
-    write(request_fifo_fd, &num_params, sizeof(int));
-    write(request_fifo_fd, &size_params, sizeof(int));
-    write(request_fifo_fd, &my_fifo_name, sizeof(char));
-    printf("Sent initial request containing client FIFO [%s]]... \n", my_fifo_name);
-    
-
-    printf("Opening client FIFO for reading... \n");
-    // Open the response/client FIFO queue for reading
-    response_fifo_fd = open(my_fifo_name, O_RDONLY);
-    if (response_fifo_fd < 0)
-    {
-        perror("open");
-        return 1;
-    }
-
-    printf("Connection to server established... \n");
-
-    return 0;
-}
-
-// TODO: <copy requirements from homework doc here>
-int client_request(int request_fifo_fd, int response_fifo_fd, int pid)
-{
-    int system_call_number;
-    int num_params;
-    int size_params;
-
-    //Send request to server
-    printf("Please choose what system call to do: \n");
-    printf("1. Connect to server\n2. Convert number to text \n3. Convert Text to Number\n");
-    printf("4. Store\n5. Recall\n0. Exit\n-1. Terminate \n");
-
-    // Read system call information from the user
-    printf("Enter system call number: \n");
-    scanf("%d", &system_call_number);
-    printf("Enter number of parameters: \n");
-    scanf("%d", &num_params);
-
-    // Calculate the size of the rest of the message
-    size_params = num_params * sizeof(int);
-
-    // Write the system call information to the request FIFO queue
-    write(request_fifo_fd, &pid, sizeof(pid_t));
-    write(request_fifo_fd, &system_call_number, sizeof(int));
-    write(request_fifo_fd, &num_params, sizeof(int));
-    write(request_fifo_fd, &size_params, sizeof(int));
-
-    // Write the parameters to the request FIFO queue
-    for (int i = 0; i < num_params; i++)
-    {
-        int param;
-        printf("Enter parameter %d: \n", i + 1);
-        scanf("%d", &param);
-        write(request_fifo_fd, &param, sizeof(int));
-    }
-    printf("Sent request");
-
-    // TODO: <how do we return system call information?>
-    return 0;
-}
-
-// EXIT - indicates THIS client does not want to issue more requests to the server,
-// it should send a “EXIT” system call to the server, 
-// close its client specific FIFO, delete it and exit.
-void exit_request(int request_fifo_fd, int response_fifo_fd, int pid)
-{
-    // hard-coded because this is exit request by client
-    int system_call_number = 0;
-    int num_params = 0;
-    int size_params = 0;
-
-    //Hard coded 1 system call
-    write(request_fifo_fd, &pid, sizeof(pid_t));
-    write(request_fifo_fd, &system_call_number, sizeof(int));
-    write(request_fifo_fd, &num_params, sizeof(int));
-    write(request_fifo_fd, &size_params, sizeof(int));
-    write(request_fifo_fd, &my_fifo_name, sizeof(int));
-    printf("Sent exit request to server... \n");
-}
-
-// TERMINATE - indicates THIS client does not want to issue more requests to the server, 
-// and is flagging the server to also exit. it should send a “TERMINATE” system call to 
-// the server, close its client specific FIFO, delete it and exit.
-void terminate_request(int request_fifo_fd, int response_fifo_fd, int pid)
-{
-    // hard-coded because this is exit request by client
-    int system_call_number = -1;
-    int num_params = 0;
-    int size_params = 0;
-
-    //Hard coded 1 system call
-    write(request_fifo_fd, &pid, sizeof(pid_t));
-    write(request_fifo_fd, &system_call_number, sizeof(int));
-    write(request_fifo_fd, &num_params, sizeof(int));
-    write(request_fifo_fd, &size_params, sizeof(int));
-    write(request_fifo_fd, &my_fifo_name, sizeof(int));
-    printf("Sent terminate request to server... \n");
 }
